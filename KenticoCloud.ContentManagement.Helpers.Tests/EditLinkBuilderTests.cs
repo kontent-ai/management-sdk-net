a887018eb6af3c4f8253abfd11a7f8e4db28f143
Peter Skoda <peters2@kentico.com>
2018-05-10 11:32:33 +0200

    CMP-629 - Added EditLinkBuilder

---
+++ KenticoCloud.ContentManagement.Helpers.Tests/EditLinkBuilderTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+using KenticoCloud.ContentManagement.Helpers.Configuration;
+
+using Xunit;
+
+namespace KenticoCloud.ContentManagement.Helpers.Tests
+{
+    public class EditLinkBuilderTests
+    {
+        private EditLinkBuilder _underTest;
+
+        public EditLinkBuilderTests()
+        {
+            _underTest = new EditLinkBuilder(new ContentManagementHelpersOptions {
+                ProjectId = "14dc0cf8-6cc1-4f20-8e2e-0b5edea89e43"
+            });
+        }
+
+        [Fact]
+        public void GetEditItemUrl_ValidInput_ReturnsValidUrl()
+        {
+            var itemId = Guid.Parse("1cdaa8ef-cb2b-4f82-82e9-45467b2e01b9");
+            var variantCodename = "some-Variant-Codename";
+
+            var result = _underTest.GetEditItemUrl(variantCodename, itemId);
+
+            var expected = "https://app.kenticocloud.com/goto/edit-item/item/1cdaa8ef-cb2b-4f82-82e9-45467b2e01b9/variant-codename/some-Variant-Codename/project/14dc0cf8-6cc1-4f20-8e2e-0b5edea89e43";
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetEditItemUrl_EmptyVariantCodename_ThrowsException()
+        {
+            var itemId = Guid.Parse("1cdaa8ef-cb2b-4f82-82e9-45467b2e01b9");
+
+            Assert.Throws<ArgumentException>(() => _underTest.GetEditItemUrl(string.Empty, itemId));
+        }
+
+        [Fact]
+        public void EditLinkBuilder_MissingEditAppEndpoint_ThrowsException()
+        {
+            var noEditAppEndpointOptions = new ContentManagementHelpersOptions
+            {
+                EditAppEndpoint = string.Empty
+            };
+
+            Assert.Throws<ArgumentException>(() => new EditLinkBuilder(noEditAppEndpointOptions));
+        }
+
+        [Fact]
+        public void EditLinkBuilder_MissingProjectId_ThrowsException()
+        {
+            var noProjectIdOptions = new ContentManagementHelpersOptions
+            {
+                ProjectId = string.Empty
+            };
+
+            Assert.Throws<ArgumentException>(() => new EditLinkBuilder(noProjectIdOptions));
+        }
+
+        [Fact]
+        public void EditLinkBuilder_ProjectIdBadFormat_ThrowsException()
+        {
+            var badFormatProjectIdOptions = new ContentManagementHelpersOptions
+            {
+                ProjectId = "aa-bb-cc"
+            };
+
+            Assert.Throws<ArgumentException>(() => new EditLinkBuilder(badFormatProjectIdOptions));
+        }   
+    }
+}
---
+++ KenticoCloud.ContentManagement.Helpers/EditLinkBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using KenticoCloud.ContentManagement.Helpers.Configuration;
+
+namespace KenticoCloud.ContentManagement.Helpers
+{
+    /// <summary>
+    /// Creates URL for redirection to editing of content items.
+    /// </summary>
+    public class EditLinkBuilder
+    {
+        private const string URL_EDIT_ITEM_WITH_VARIANT_CODENAME = "goto/edit-item/item/{0}/variant-codename/{1}/project/{2}";
+
+        private readonly ContentManagementHelpersOptions _options;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EditLinkBuilder"/> class for retrieving edit urls.
+        /// </summary>
+        /// <param name="contentManagementHelpersOptions">The settings of the Kentico Cloud project.</param>
+        public EditLinkBuilder(ContentManagementHelpersOptions contentManagementHelpersOptions)
+        {
+            if (string.IsNullOrEmpty(contentManagementHelpersOptions.EditAppEndpoint))
+            {
+                throw new ArgumentException("Kentico Cloud Edit App endpoint is not specified.", nameof(contentManagementHelpersOptions.EditAppEndpoint));
+            }
+
+            if (string.IsNullOrEmpty(contentManagementHelpersOptions.ProjectId))
+            {
+                throw new ArgumentException("Kentico Cloud project identifier is not specified.", nameof(contentManagementHelpersOptions.ProjectId));
+            }
+
+            if (!Guid.TryParse(contentManagementHelpersOptions.ProjectId, out Guid projectIdGuid))
+            {
+                throw new ArgumentException($"Provided string is not a valid project identifier ({contentManagementHelpersOptions.ProjectId}).", nameof(contentManagementHelpersOptions.ProjectId));
+            }
+
+            _options = contentManagementHelpersOptions;
+        }
+
+        /// <summary>
+        /// Gets URL to edit page of specified content item.
+        /// </summary>
+        /// <param name="variantCodename">Codename of variant.</param>
+        /// <param name="itemId">Unique identifier of content item.</param>
+        /// <returns>URL to edit page of specified item.</returns>
+        public string GetEditItemUrl(string variantCodename, Guid itemId)
+        {
+            if (string.IsNullOrEmpty(variantCodename))
+            {
+                throw new ArgumentException("Variant codename is not specified.", nameof(variantCodename));
+            }
+
+            return string.Format(_options.EditAppEndpoint,
+                string.Format(URL_EDIT_ITEM_WITH_VARIANT_CODENAME, itemId, variantCodename, _options.ProjectId));
+        }
+    }
+}
