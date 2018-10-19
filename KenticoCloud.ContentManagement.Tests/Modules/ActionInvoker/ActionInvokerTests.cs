0276cf4cd8461dd7c99bddeb8672b6f4749a6124
Haaxor1689 <betkomaros@gmail.com>
2018-10-19 13:48:55 +0200

    Added unit test for decimal 0 serialization.

---
+++ KenticoCloud.ContentManagement.Tests/Modules/ActionInvoker/ActionInvokerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using KenticoCloud.ContentManagement.Models.Items;
+using KenticoCloud.ContentManagement.Modules.ActionInvoker;
+using KenticoCloud.ContentManagement.Modules.HttpClient;
+
+using Xunit;
+
+namespace KenticoCloud.ContentManagement.Tests
+{
+    internal class FakeContentManagementHttpClient : IContentManagementHttpClient {
+        internal string requestBody;
+
+        public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage message) {
+            requestBody = await message.Content.ReadAsStringAsync();
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+    }
+
+    public class ActionInvokerTests
+    {
+        [Fact]
+        [Trait("Issue", "29")]
+        public void ActionInvokerSerializeElementContainingZero_SerializedJsonContainsZero() {
+            var httpClient = new FakeContentManagementHttpClient();
+            var actionInvoker = new ActionInvoker(httpClient, new MessageCreator("{api_key}"));
+            
+            var contentItemVariantUpsertModel = new ContentItemVariantUpsertModel() {
+                Elements = new {
+                    number = new decimal(0),
+                },
+            };
+
+            var result = actionInvoker.InvokeMethodAsync<ContentItemVariantUpsertModel, dynamic>("{endpoint_url}", HttpMethod.Get,contentItemVariantUpsertModel);
+            Assert.Equal("{\"elements\":{\"number\":0}}", httpClient.requestBody);
+        }
+    }
+}
