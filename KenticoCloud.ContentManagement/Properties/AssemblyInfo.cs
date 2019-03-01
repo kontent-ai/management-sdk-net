cb75a26cc677f3ee7fd4a752d631b9d7fe9a03c4
Petr Švihlík <svihlik.petr@gmail.com>
2019-03-01 16:18:19 +0100

    adjusted version management, added symbol generation

--- KenticoCloud.ContentManagement/Properties/AssemblyInfo.cs
+++ KenticoCloud.ContentManagement/Properties/AssemblyInfo.cs
@@ -1,16 +1,8 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("KenticoCloud.ContentManagement")]
-[assembly: AssemblyDescription("Kentico Cloud Content Management SDK")]
-[assembly: AssemblyCompany("Kentico Software")]
-[assembly: AssemblyProduct("Kentico Cloud")]
-[assembly: AssemblyCopyright("© 2017 Kentico Software. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c05d1ba8-e89e-429d-aaef-e3bec5a909ae")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("KenticoCloud.ContentManagement.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
