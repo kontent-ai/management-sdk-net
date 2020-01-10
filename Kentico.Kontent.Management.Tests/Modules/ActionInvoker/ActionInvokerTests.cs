f794423a060ee10b1427d8d95fd8b943b29bbf2c
ZdenekJ <zdenekj@kentico.com>
2020-01-10 12:41:10 +0100

    KCL-3591 - Use StringEnumConverter in request body serialization

--- Kentico.Kontent.Management.Tests/Modules/ActionInvoker/ActionInvokerTests.cs
+++ Kentico.Kontent.Management.Tests/Modules/ActionInvoker/ActionInvokerTests.cs
@@ -1,9 +1,11 @@
-﻿using Kentico.Kontent.Management.Models.Items;
+﻿using System;
+using Kentico.Kontent.Management.Models.Items;
 using Kentico.Kontent.Management.Modules.ActionInvoker;
 using Kentico.Kontent.Management.Modules.HttpClient;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Kentico.Kontent.Management.Models.Assets;
 using Xunit;
 
 namespace Kentico.Kontent.Management.Tests
@@ -16,7 +18,10 @@ namespace Kentico.Kontent.Management.Tests
         {
             var message = messageCreator.CreateMessage(method, endpointUrl, content);
             requestBody = await message.Content.ReadAsStringAsync();
-            return new HttpResponseMessage(HttpStatusCode.Accepted);
+            return new HttpResponseMessage(HttpStatusCode.Accepted)
+            {
+                Content = new StringContent(requestBody)
+            };
         }
     }
 
@@ -26,7 +31,7 @@ namespace Kentico.Kontent.Management.Tests
         [Trait("Issue", "29")]
         [InlineData(0.0, "0")]
         [InlineData(29.0, "29.0")]
-        public void ActionInvokerSerializeElementContainingZero_SerializedJsonContainsZero(decimal d, string s)
+        public async Task ActionInvokerSerializeElementContainingZero_SerializedJsonContainsZero(decimal d, string s)
         {
             var httpClient = new FakeManagementHttpClient();
             var actionInvoker = new ActionInvoker(httpClient, new MessageCreator("{api_key}"));
@@ -40,8 +45,38 @@ namespace Kentico.Kontent.Management.Tests
                 },
             };
 
-            var result = actionInvoker.InvokeMethodAsync<ContentItemVariantUpsertModel, dynamic>("{endpoint_url}", HttpMethod.Get, contentItemVariantUpsertModel);
+            await actionInvoker.InvokeMethodAsync<ContentItemVariantUpsertModel, dynamic>("{endpoint_url}", HttpMethod.Get, contentItemVariantUpsertModel);
             Assert.Equal($"{{\"elements\":{{\"zero\":{s},\"optZero\":{s}}}}}", httpClient.requestBody);
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            var httpClient = new FakeManagementHttpClient();
+            var actionInvoker = new ActionInvoker(httpClient, new MessageCreator("{api_key}"));
+            
+            var assetUpsertModel = new AssetUpsertModel()
+            {
+                Title = "Asset",
+                Descriptions = new []
+                {
+                    new AssetDescription()
+                    {
+                        Description = "Description",
+                        Language = LanguageIdentifier.DEFAULT_LANGUAGE
+                    }, 
+                },
+                FileReference = new FileReference()
+                {
+                    Id = "ab7bdf75-781b-4bf9-aed8-501048860402",
+                    Type = FileReferenceTypeEnum.Internal
+                }
+            };
+
+            await actionInvoker.InvokeMethodAsync<AssetUpsertModel, dynamic>("{endpoint_url}", HttpMethod.Put, assetUpsertModel);
+
+            var expectedRequestBody = "{\"file_reference\":{\"id\":\"ab7bdf75-781b-4bf9-aed8-501048860402\",\"type\":\"internal\"},\"descriptions\":[{\"language\":{\"id\":\"00000000-0000-0000-0000-000000000000\"},\"description\":\"Description\"}],\"title\":\"Asset\"}";
+            Assert.Equal(expectedRequestBody, httpClient.requestBody);
         }
     }
 }
