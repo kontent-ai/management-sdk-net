3fe44687bc50e8334e20b212cc8e5b8207962d6e
Ondřej Chrastina <chrastina.ondra@gmail.com>
2021-07-23 11:33:07 +0200

    add test

---
+++ Kentico.Kontent.Management.Tests/Modules/Extensions/PropertyInfoExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+
+using Kentico.Kontent.Management.Modules.HttpClient;
+using Kentico.Kontent.Management.Modules.Extensions;
+
+using Xunit;
+using Kentico.Kontent.Management.Modules.ModelBuilders;
+
+namespace Kentico.Kontent.Management.Tests
+{
+
+    public class PropertyInfoExtensionsTests
+    {
+        internal const string ELEMENT_ID_GUID = "632afb85-9b1a-46aa-9717-5991ae859e13";
+        internal class PropertyInfoExtensionsTestsSampleClass
+        {
+            public string Property1 { get; set; }
+
+            [KontentElementId(ELEMENT_ID_GUID)]
+            public string Property2 { get; set; }
+        }
+
+        [Fact]
+        public void GetKontentElementId_ReturnsNullIfNoAttribute()
+        {
+            var property = typeof(PropertyInfoExtensionsTestsSampleClass).GetProperty("Property1");
+
+            Assert.Null(property.GetKontentElementId());
+        }
+
+        [Fact]
+        public void GetKontentElementId_ReturnsAttributeValue()
+        {
+            var property = typeof(PropertyInfoExtensionsTestsSampleClass).GetProperty("Property2");
+
+            Assert.Equal(ELEMENT_ID_GUID, property.GetKontentElementId());
+        }
+    }
+}
