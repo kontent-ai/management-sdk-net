4ab60345b69edbb810eb41d9263b5d3fb8a80f64
Ondrej Chrastina <chrastina.ondra@gmail.com>
2021-10-21 13:12:49 +0200

    extract common configuration to test ficture container

--- Kentico.Kontent.Management.Tests/Unit/ManagementClientTests/FileSystemFixture.cs
+++ Kentico.Kontent.Management.Tests/Unit/ManagementClientTests/FileSystemFixture.cs
@@ -13,27 +13,33 @@ namespace Kentico.Kontent.Management.Tests.Unit.ManagementClientTests
         /// ID of the test project.
         /// </summary>
         public const string PROJECT_ID = "a9931a80-9af4-010b-0590-ecb1273cf1b8";
-        
-        protected ManagementClient _client;
+        private IConfiguration _configuration;
+        private ManagementOptions _managementOptions;
+        private EndpointUrlBuilder _urlBuilder;
+        private MessageCreator _messageCreator;
 
-        protected IManagementHttpClient HttpClient { get; private set; }
-
-        public void setMockedHttpClient(IManagementHttpClient httpClient)
+        public FileSystemFixture()
         {
-
-            var configuration = new ConfigurationBuilder()
+            _configuration = new ConfigurationBuilder()
                 .AddUserSecrets<FileSystemFixture>()
                 .Build();
-            var options = new ManagementOptions() { ApiKey = configuration.GetValue<string>("ApiKey") ?? "Dummy_API_key", ProjectId = configuration.GetValue<string>("ProjectId") ?? PROJECT_ID };
-            var urlBuilder = new EndpointUrlBuilder(options);
-            var actionInvoker = new ActionInvoker(httpClient, new MessageCreator(options.ApiKey));
+            _managementOptions = new ManagementOptions()
+            {
+                ApiKey = _configuration.GetValue<string>("ApiKey", "Dummy_API_key"),
+                ProjectId = _configuration.GetValue<string>("ProjectId", PROJECT_ID)
+            };
+            _urlBuilder = new EndpointUrlBuilder(_managementOptions);
+            _messageCreator = new MessageCreator(_managementOptions.ApiKey);
+        }
 
-            _client = new ManagementClient(urlBuilder, actionInvoker);
+        public ManagementClient CreateMockClient(IManagementHttpClient httpClient)
+        {
+            var actionInvoker = new ActionInvoker(httpClient, _messageCreator);
+            return new ManagementClient(_urlBuilder, actionInvoker);
         }
 
         public void Dispose()
         {
-            _client = null;
         }
     }
 }
