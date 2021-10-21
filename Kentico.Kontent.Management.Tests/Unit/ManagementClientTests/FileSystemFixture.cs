78cb446c18221263b68e9876d5735a3c80bb5a58
Ondrej Chrastina <chrastina.ondra@gmail.com>
2021-10-21 07:52:06 +0200

    code cleanup

--- Kentico.Kontent.Management.Tests/Unit/ManagementClientTests/FileSystemFixture.cs
+++ Kentico.Kontent.Management.Tests/Unit/ManagementClientTests/FileSystemFixture.cs
@@ -1,46 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Kentico.Kontent.Management;
-using Kentico.Kontent.Management.Models.Languages;
-using Kentico.Kontent.Management.Models.Shared;
 using Kentico.Kontent.Management.Modules.ActionInvoker;
 using Kentico.Kontent.Management.Modules.HttpClient;
-using Kentico.Kontent.Management.Tests.Mocks;
-using Kentico.Kontent.Management.Tests.Unit.ManagementClientTests;
 using Kentico.Kontent.Management.UrlBuilder;
 using Microsoft.Extensions.Configuration;
-using Xunit;
 
 namespace Kentico.Kontent.Management.Tests.Unit.ManagementClientTests
 {
-
-    // public interface IHttpResponseMessageToJsonMapper
-    // {
-    //     Task<HttpResponseMessage> GetHttpResponseMessageAsync(HttpMethod method, string endpointUrl);
-    // }
-
-    // public class FileSystemManagementBase : IManagementHttpClient
-    // {
-    //     private IHttpResponseMessageToJsonMapper _mapper;
-    //     public FileSystemManagementBase(IHttpResponseMessageToJsonMapper mapper)
-    //     {
-    //         _mapper = mapper;
-    //     }
-
-    //     public Task<HttpResponseMessage> SendAsync(IMessageCreator messageCreator, string endpointUrl, HttpMethod method, HttpContent content = null, Dictionary<string, string> headers = null)
-    //     {
-    //         if (_mapper == null)
-    //         {
-    //             Assert.True(false, "Please provide an implementation of the IHttpResponseMessageToJsonMapper (mapper property) to the test fixture.");
-    //             return null;
-    //         }
-
-    //         return _mapper.GetHttpResponseMessageAsync(method, endpointUrl);
-    //     }
-
-    // }
     public class FileSystemFixture : IDisposable
     {
 
