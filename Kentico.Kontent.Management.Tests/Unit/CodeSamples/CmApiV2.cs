a8bf02a9e57f2ae4fe1623eada3135cd924be06a
Ondřej Chrastina <chrastina.ondra@gmail.com>
2021-11-01 16:55:30 +0100

    first batch of GetX methods
    https://github.com/KenticoDocs/kontent-docs-samples/tree/master/rest/cm-api-v2

--- Kentico.Kontent.Management.Tests/Unit/CodeSamples/CmApiV2.cs
+++ Kentico.Kontent.Management.Tests/Unit/CodeSamples/CmApiV2.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Kentico.Kontent.Management.Models.LanguageVariants;
 using Kentico.Kontent.Management.Models.Shared;
 using Kentico.Kontent.Management.Modules.HttpClient;
@@ -114,6 +115,92 @@ namespace Kentico.Kontent.Management.Tests.Unit.CodeSamples
             var identifier = Reference.ById(Guid.Parse("d53360f7-79e1-42f4-a524-1b53a417d03e"));
 
             await client.DeleteWebhookAsync(identifier);
+        }
+
+        // DocSection: cm_api_v2_get_asset
+        // Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
+        [Fact]
+        public async void GetAsset()
+        {
+            var client = _fileSystemFixture.CreateDefaultMockClientRespondingWithFilename("Asset.json");
+
+            var identifier = Reference.ById(Guid.Parse("fcbb12e6-66a3-4672-85d9-d502d16b8d9c"));
+            // var identifier = Reference.ByCodename("which-brewing-fits-you");
+
+            var response = await client.GetAssetAsync(identifier);
+
+            Assert.NotNull(response);
+        }
+
+        // DocSection: cm_api_v2_get_assets
+        // Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
+        [Fact]
+        public async void GetAssets()
+        {
+            var client = _fileSystemFixture.CreateDefaultMockClientRespondingWithFilename("Assets.json");
+
+            var response = await client.ListAssetsAsync();
+
+            Assert.Single(response);
+        }
+
+        // DocSection: cm_api_v2_get_components_of_type
+        // Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
+        [Fact]
+        public async void GetComponentsOfType()
+        {
+            var client = _fileSystemFixture.CreateDefaultMockClientRespondingWithFilename("ContentItemsWithComponents.json");
+
+            var identifier = Reference.ById(Guid.Parse("6434e475-5a29-4866-9fd1-6d1ca873f5be"));
+            // var identifier = Reference.ByCodename("article");
+            // var identifier = Reference.ByExternalId("my-article-id");
+
+            var response = await client.ListLanguageVariantsOfContentTypeWithComponentsAsync(identifier);
+
+            Assert.NotNull(response);
+        }
+
+        // DocSection: cm_api_v2_get_content_collections
+        // Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
+        [Fact]
+        public async void GetContentCollections()
+        {
+            var client = _fileSystemFixture.CreateDefaultMockClientRespondingWithFilename("Collections.json");
+
+            var response = await client.ListCollectionsAsync();
+
+            Assert.NotEmpty(response.Collections); 
+            Assert.Equal(2, response.Collections.Count());
+        }
+
+        // DocSection: cm_api_v2_get_asset_folders
+        // Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
+        [Fact]
+        public async void GetFolders()
+        {
+            var client = _fileSystemFixture.CreateDefaultMockClientRespondingWithFilename("AssetFolders.json");
+
+            var response = await client.GetAssetFoldersAsync();
+
+            Assert.NotEmpty(response.Folders); 
+            Assert.Equal(2, response.Folders.Count());
+            Assert.Single(response.Folders.First().Folders);
+        }
+
+        // DocSection: cm_api_v2_get_item
+        // Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
+        [Fact]
+        public async void GetItem()
+        {
+            var client = _fileSystemFixture.CreateDefaultMockClientRespondingWithFilename("ContentItem.json");
+
+            var identifier = Reference.ById(Guid.Parse("f4b3fc05-e988-4dae-9ac1-a94aba566474"));
+            // var identifier = Reference.ByCodename("my_article");
+            // var identifier = Reference.ByExternalId("59713");
+
+            var response = await client.GetContentItemAsync(identifier);
+
+            Assert.NotNull(response);
         }
     }
 }
