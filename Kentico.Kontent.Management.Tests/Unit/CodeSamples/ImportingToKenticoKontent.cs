6e28c3d3bae890197a0c80a2a04a93825d5a26ef
Ondřej Chrastina <chrastina.ondra@gmail.com>
2021-11-12 11:21:08 +0100

    add info about code samples

--- Kentico.Kontent.Management.Tests/Unit/CodeSamples/ImportingToKenticoKontent.cs
+++ Kentico.Kontent.Management.Tests/Unit/CodeSamples/ImportingToKenticoKontent.cs
@@ -10,8 +10,13 @@ using Xunit;
 
 namespace Kentico.Kontent.Management.Tests.Unit.CodeSamples
 {
+        /// <summary>
+    /// Source for Code examples being store in https://github.com/KenticoDocs/kontent-docs-samples/tree/master/net/importing-to-kentico-kontent
+    /// </summary>
     public class ImportingToKenticoKontent : IClassFixture<FileSystemFixture>
     {
+        // IF YOU MAKE ANY CHANGE TO THIS FILE - ADJUST THE CODE SAMPLES
+
         private FileSystemFixture _fileSystemFixture;
 
         public ImportingToKenticoKontent(FileSystemFixture fileSystemFixture)
