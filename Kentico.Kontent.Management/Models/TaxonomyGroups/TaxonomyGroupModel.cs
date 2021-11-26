9c89d32020edfd7948981e9d9f025fdd316d522e
Ondřej Chrastina <chrastina.ondra@gmail.com>
2021-11-26 10:58:19 +0100

    refactor taxonomy group ingeritance structure

--- Kentico.Kontent.Management/Models/TaxonomyGroups/TaxonomyGroupModel.cs
+++ Kentico.Kontent.Management/Models/TaxonomyGroups/TaxonomyGroupModel.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 
 namespace Kentico.Kontent.Management.Models.TaxonomyGroups
 {
@@ -9,9 +10,21 @@ namespace Kentico.Kontent.Management.Models.TaxonomyGroups
     public class TaxonomyGroupModel : TaxonomyBaseModel
     {
         /// <summary>
+        /// Gets or sets the taxonomy group's internal ID.
+        /// </summary>
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        /// <summary>
         /// Gets or sets ISO-8601 formatted date/time of the last change to the taxonomy group or its terms.
         /// </summary>
         [JsonProperty("last_modified")]
         public DateTime? LastModified { get; set; }
+
+        /// <summary>
+        /// Gets or sets terms in the taxonomy group.
+        /// </summary>
+        [JsonProperty("terms")]
+        public IEnumerable<TaxonomyTermModel> Terms { get; set; }
     }
 }
--- Kentico.Kontent.Management/Models/TaxonomyGroups/TaxonomyTermCreateModel.cs
+++ Kentico.Kontent.Management/Models/TaxonomyGroups/TaxonomyTermCreateModel.cs
@@ -1,9 +1,17 @@
-﻿namespace Kentico.Kontent.Management.Models.TaxonomyGroups
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Kentico.Kontent.Management.Models.TaxonomyGroups
 {
     /// <summary>
     /// Represents the taxonomy term create model.
     /// </summary>
-    public class TaxonomyTermCreateModel : TaxonomyGroupCreateModel
+    public class TaxonomyTermCreateModel : TaxonomyBaseModel
     {
+        /// <summary>
+        /// Gets or sets terms in the taxonomy group.
+        /// </summary>
+        [JsonProperty("terms")]
+        public IEnumerable<TaxonomyTermCreateModel> Terms { get; set; }
     }
 }
