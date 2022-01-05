13efc4401ef044fd50883f81e837667a8b8d1745
Martin Ševčík <sevcik.martin4@gmail.com>
2022-01-05 11:16:22 +0100

    53 add missing documentation

--- Kentico.Kontent.Management/Models/Shared/IListingResponse.cs
+++ Kentico.Kontent.Management/Models/Shared/IListingResponse.cs
@@ -2,8 +2,15 @@
 
 namespace Kentico.Kontent.Management.Models.Shared
 {
+    /// <summary>
+    /// Defines the contract for listing responses
+    /// </summary>
+    /// <typeparam name="T">Represents paginated response model</typeparam>
     public interface IListingResponse<T> : IEnumerable<T>
     {
+        /// <summary>
+        /// Represents pagination object
+        /// </summary>
         PaginationResponseModel Pagination { get; set; }
     }
 }
