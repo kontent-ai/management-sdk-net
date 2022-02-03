c045ac917c2f5c4d826b1e09c05f8e16999dc445
Jiri Kotasek <kotas24@gmail.com>
2022-02-03 16:18:06 +0100

    Subscription models

---
+++ Kentico.Kontent.Management/Models/Subscription/SubscriptionColletionGroupModel.cs
@@ -0,0 +1,25 @@
+﻿using Kentico.Kontent.Management.Models.Shared;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Kentico.Kontent.Management.Models.Subscription
+{
+    /// <summary>
+    /// Represents set of collections user is assigned to with a set of roles.
+    /// </summary>
+    public sealed class SubscriptionColletionGroupModel
+    {
+        /// <summary>
+        /// Gets or sets references to internal identifiers of collections that the user is assigned to. 
+        /// If the array is empty, the user can access any collection.
+        /// </summary>
+        [JsonProperty("collections")]
+        public IEnumerable<Reference> Collections { get; set; }
+
+        /// <summary>
+        /// Gets or sets roles the user is assigned to within the collection.
+        /// </summary>
+        [JsonProperty("roles")]
+        public IEnumerable<SubscriptionUserRoleModel> Roles { get; set; }
+    }
+}
