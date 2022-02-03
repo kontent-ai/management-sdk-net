7c2e8ebd7b4c887c8c6f187ac8ff0b783a116704
Jiri Kotasek <kotas24@gmail.com>
2022-02-03 17:15:25 +0100

    Implement subscription endpoints

--- Kentico.Kontent.Management/ManagementClient.cs
+++ Kentico.Kontent.Management/ManagementClient.cs
@@ -29,6 +29,7 @@ using Kentico.Kontent.Management.Models.Assets.Patch;
 using Kentico.Kontent.Management.UrlBuilder;
 using Kentico.Kontent.Management.Models.Roles;
 using Kentico.Kontent.Management.Models.Users;
+using Kentico.Kontent.Management.Models.Subscription;
 
 namespace Kentico.Kontent.Management
 {
@@ -1004,6 +1005,79 @@ namespace Kentico.Kontent.Management
 
             var endpointUrl = _urlBuilder.BuildProjectRoleUrl(identifier);
             return await _actionInvoker.InvokeReadOnlyMethodAsync<ProjectRoleModel>(endpointUrl, HttpMethod.Get);
+        }
+
+        /// <inheritdoc />
+        public async Task<SubscriptionProjectModel> GetSubscriptionProjectAsync(Guid projectId)
+        {
+            var endpointUrl = _urlBuilder.BuildSubscriptionProjectUrl(projectId);
+
+            return await _actionInvoker.InvokeReadOnlyMethodAsync<SubscriptionProjectModel>(endpointUrl, HttpMethod.Get);
+        }
+
+        /// <inheritdoc />
+        public async Task<IListingResponseModel<SubscriptionProjectModel>> ListSubscriptionProjectsAsync()
+        {
+            var endpointUrl = _urlBuilder.BuildSubscriptionProjectsUrl();
+            var response = await _actionInvoker.InvokeReadOnlyMethodAsync<SubscriptionProjectListingResponseServerModel>(endpointUrl, HttpMethod.Get);
+
+            return new ListingResponseModel<SubscriptionProjectModel>(
+                (token, url) => GetNextListingPageAsync<SubscriptionProjectListingResponseServerModel, SubscriptionProjectModel>(token, url),
+                response.Pagination?.Token,
+                endpointUrl,
+                response.Projects);
+        }
+
+        /// <inheritdoc />
+        public async Task<IListingResponseModel<SubscriptionUserModel>> ListSubscriptionUsersAsync()
+        {
+            var endpointUrl = _urlBuilder.BuildSubscriptionUsersUrl();
+            var response = await _actionInvoker.InvokeReadOnlyMethodAsync<SubscriptionUserListingResponseServerModel>(endpointUrl, HttpMethod.Get);
+
+            return new ListingResponseModel<SubscriptionUserModel>(
+                (token, url) => GetNextListingPageAsync<SubscriptionUserListingResponseServerModel, SubscriptionUserModel>(token, url),
+                response.Pagination?.Token,
+                endpointUrl,
+                response.Users);
+        }
+
+        /// <inheritdoc />
+        public async Task<SubscriptionUserModel> GetSubscriptionUserAsync(UserIdentifier identifier)
+        {
+            if (identifier == null)
+            {
+                throw new ArgumentNullException(nameof(identifier));
+            }
+
+            var endpointUrl = _urlBuilder.BuildSubscriptionUserUrl(identifier);
+
+            return await _actionInvoker.InvokeReadOnlyMethodAsync<SubscriptionUserModel>(endpointUrl, HttpMethod.Get);
+        }
+
+        /// <inheritdoc />
+        public async Task ActivateSubscriptionUserAsync(UserIdentifier identifier)
+        {
+            if (identifier == null)
+            {
+                throw new ArgumentNullException(nameof(identifier));
+            }
+
+            var endpointUrl = _urlBuilder.BuildSubscriptionUserActivateUrl(identifier);
+
+            await _actionInvoker.InvokeMethodAsync(endpointUrl, HttpMethod.Put);
+        }
+
+        /// <inheritdoc />
+        public async Task DeactivateSubscriptionUserAsync(UserIdentifier identifier)
+        {
+            if (identifier == null)
+            {
+                throw new ArgumentNullException(nameof(identifier));
+            }
+
+            var endpointUrl = _urlBuilder.BuildSubscriptionUserDeactivateDisableUrl(identifier);
+
+            await _actionInvoker.InvokeMethodAsync(endpointUrl, HttpMethod.Put);
         }
 
         private async Task<IListingResponse<TModel>> GetNextListingPageAsync<TListingResponse, TModel>(string continuationToken, string url)
