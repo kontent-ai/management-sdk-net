50b22bf8d586e8d6d0d8fcde10112ebb86caecd1
Jiri Kotasek <kotas24@gmail.com>
2022-02-03 16:18:06 +0100

    Subscription models

---
+++ Kentico.Kontent.Management/Models/Subscription/SubscriptionProjectModel.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Kentico.Kontent.Management.Models.Subscription
+{
+    /// <summary>
+    /// Represents the subscription project object.
+    /// </summary>
+    public sealed class SubscriptionProjectModel
+    {
+        /// <summary>
+        /// Gets or sets the id of the project.
+        /// </summary>
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the project's name.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a flag determining whether the project is active.
+        /// </summary>
+        [JsonProperty("is_active")]
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Gets or sets the project's environments.
+        /// </summary>
+        [JsonProperty("environments")]
+        public IEnumerable<SubscriptionEnvironmentModel> Environmnets { get; set; }
+    }
+}
---
+++ Kentico.Kontent.Management/Models/Subscription/SubscriptionUserModel.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Kentico.Kontent.Management.Models.Subscription
+{
+    /// <summary>
+    /// Represents subscription user object.
+    /// </summary>
+    public sealed class SubscriptionUserModel
+    {
+        /// <summary>
+        /// Gets or sets the id of the user.
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's name.
+        /// </summary>
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's name.
+        /// </summary>
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's name.
+        /// </summary>
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's name.
+        /// </summary>
+        [JsonProperty("has_pending_invitation")]
+        public bool HasPendingInvitation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's name.
+        /// </summary>
+        [JsonProperty("projects")]
+        public IEnumerable<SubscriptionUserProjectModel> Projects { get; set; }
+    }
+}
