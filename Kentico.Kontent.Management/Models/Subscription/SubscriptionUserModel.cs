e27c4b7c3ad7dba9ae36ca2639134c2dbc5ec26d
Jiri Kotasek <kotas24@gmail.com>
2022-02-24 19:30:53 +0100

    Fix xml documentation

--- Kentico.Kontent.Management/Models/Subscription/SubscriptionUserModel.cs
+++ Kentico.Kontent.Management/Models/Subscription/SubscriptionUserModel.cs
@@ -9,37 +9,37 @@ namespace Kentico.Kontent.Management.Models.Subscription
     public sealed class SubscriptionUserModel
     {
         /// <summary>
-        /// Gets or sets the id of the user.
+        /// Gets or sets the user's internal ID.
         /// </summary>
         [JsonProperty("id")]
         public string Id { get; set; }
 
         /// <summary>
-        /// Gets or sets the user's name.
+        /// Gets or sets the user's first name.
         /// </summary>
         [JsonProperty("first_name")]
         public string FirstName { get; set; }
 
         /// <summary>
-        /// Gets or sets the user's name.
+        /// Gets or sets the user's last name.
         /// </summary>
         [JsonProperty("last_name")]
         public string LastName { get; set; }
 
         /// <summary>
-        /// Gets or sets the user's name.
+        /// Gets or sets the user's email address.
         /// </summary>
         [JsonProperty("email")]
         public string Email { get; set; }
 
         /// <summary>
-        /// Gets or sets the user's name.
+        /// Gets or sets a flag determining whether the user has any pending invitation to a project.
         /// </summary>
         [JsonProperty("has_pending_invitation")]
         public bool HasPendingInvitation { get; set; }
 
         /// <summary>
-        /// Gets or sets the user's name.
+        /// Gets or sets the projects to which the user has been invited.
         /// </summary>
         [JsonProperty("projects")]
         public IEnumerable<SubscriptionUserProjectModel> Projects { get; set; }
