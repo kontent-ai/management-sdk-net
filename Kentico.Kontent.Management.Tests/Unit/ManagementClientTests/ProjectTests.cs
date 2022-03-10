cbf5ea3ad071ffac5be3b8430f339b2e8fbc85c9
jirik2 <jirik2@kentico.com>
2022-03-10 17:31:00 +0100

    Add unit tests for project and project validation

---
+++ Kentico.Kontent.Management.Tests/Unit/ManagementClientTests/ProjectTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Kentico.Kontent.Management.Models.ProjectReport;
+using Kentico.Kontent.Management.Tests.Unit.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kentico.Kontent.Management.Tests.Unit.ManagementClientTests
+{
+    public class ProjectTests : IClassFixture<FileSystemFixture>
+    {
+        private FileSystemFixture _fileSystemFixture;
+
+        public ProjectTests(FileSystemFixture fileSystemFixture)
+        {
+            _fileSystemFixture = fileSystemFixture;
+            _fileSystemFixture.SetSubFolder("Project");
+        }
+
+        [Fact]
+        public async Task GetProjectInfo_GetsProjectInfo()
+        {
+            var client = _fileSystemFixture.CreateMockClientWithResponse("Project.json");
+
+            var expected = _fileSystemFixture.GetExpectedResponse<Project>("Project.json");
+
+            var response = await client.GetProjectInformationAsync();
+
+            response.Should().BeEquivalentTo(expected);
+        }
+    }
+}
