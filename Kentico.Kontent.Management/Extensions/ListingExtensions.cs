70760822c3b4413e149e8b12b95ab8d4dec1e908
jirik2 <jirik2@kentico.com>
2022-03-18 10:39:57 +0100

    Order modifiers

--- Kentico.Kontent.Management/Extensions/ListingExtensions.cs
+++ Kentico.Kontent.Management/Extensions/ListingExtensions.cs
@@ -18,7 +18,7 @@ namespace Kentico.Kontent.Management.Extensions
         /// <typeparam name="T">Entity model e.g <see cref="LanguageModel"/>, <see cref="TaxonomyGroupModel"/> and etc.</typeparam>
         /// <param name="method">To be extended method that returns <see cref="Task{IListingResponseModel}"/></param>
         /// <returns><see cref="List{T}"/> all items for project</returns>
-        public static async Task<List<T>> GetAllAsync<T>(this Task<IListingResponseModel<T>> method)
+        public async static Task<List<T>> GetAllAsync<T>(this Task<IListingResponseModel<T>> method)
         {
             var result = new List<T>();
             var response = await method;
