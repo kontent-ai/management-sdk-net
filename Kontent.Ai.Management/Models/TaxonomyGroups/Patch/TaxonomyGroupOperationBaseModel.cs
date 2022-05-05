69e78deab6d7e2acb7afbc85f443fc55f7de2e8e
Jiri Kotasek <kotas24@gmail.com>
2022-05-05 13:52:45 +0200

    Remove Required from operation base models
    (cherry picked from commit c69397429d1a9b74969934bcd9e4eed7cf0fafc0)

--- Kontent.Ai.Management/Models/TaxonomyGroups/Patch/TaxonomyGroupOperationBaseModel.cs
+++ Kontent.Ai.Management/Models/TaxonomyGroups/Patch/TaxonomyGroupOperationBaseModel.cs
@@ -13,13 +13,13 @@ public abstract class TaxonomyGroupOperationBaseModel
     /// Gets specification of the operation to perform.
     /// More info: https://kontent.ai/learn/reference/management-api-v2#operation/modify-a-taxonomy-group
     /// </summary>
-    [JsonProperty("op", Required = Required.Always)]
+    [JsonProperty("op")]
     public abstract string Op { get; }
 
     /// <summary>
     /// Gets or sets the reference to existing taxonomy terms you want to modify.
     /// </summary>
-    [JsonProperty("reference", Required = Required.Always)]
+    [JsonProperty("reference")]
     public Reference Reference { get; set; }
 
 }
