30a9e45cafe6aa2fca6189a1fe016cafb139d56b
Jiri Kotasek <kotas24@gmail.com>
2022-05-05 13:53:26 +0200

    unit tests - taxonomy group

--- Kentico.Kontent.Management.Tests/Base/Converters/ConverterHelper.cs
+++ Kentico.Kontent.Management.Tests/Base/Converters/ConverterHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kentico.Kontent.Management.Models.TaxonomyGroups.Patch;
+using Kentico.Kontent.Management.Models.Types.Patch;
+using System;
 using System.Collections;
 using System.Linq;
 
@@ -10,4 +12,12 @@ internal static class ConverterHelper
         typeof(IEnumerable).IsAssignableFrom(objectType.GetGenericTypeDefinition()) &&
         objectType.GenericTypeArguments.Length == 1 &&
         objectType.GenericTypeArguments.FirstOrDefault() == typeof(T);
+
+    public static bool IsOperationModel<T>() =>
+        typeof(T) == typeof(TaxonomyGroupOperationBaseModel) ||
+        typeof(T) == typeof(ContentTypeOperationBaseModel);
+
+    public static bool HasProperty<T>(string propertyName) => typeof(T).GetProperty(propertyName) != null;
+
+    public static object GetPropertyValue(this object obj, string propName) => obj.GetType().GetProperty(propName).GetValue(obj, null);
 }
