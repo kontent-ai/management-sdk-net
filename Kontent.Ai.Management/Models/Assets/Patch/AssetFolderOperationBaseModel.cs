8227fd9186d5e27c64d7494492daed034947923f
Jiri Kotasek <kotas24@gmail.com>
2022-05-05 13:52:45 +0200

    Remove Required from operation base models
    (cherry picked from commit c69397429d1a9b74969934bcd9e4eed7cf0fafc0)

--- Kontent.Ai.Management/Models/Assets/Patch/AssetFolderOperationBaseModel.cs
+++ Kontent.Ai.Management/Models/Assets/Patch/AssetFolderOperationBaseModel.cs
@@ -11,13 +11,13 @@ public abstract class AssetFolderOperationBaseModel
     /// <summary>
     /// Gets specification of the operation to perform.
     /// </summary>
-    [JsonProperty("op", Required = Required.Always)]
+    [JsonProperty("op")]
     public abstract string Op { get; }
 
     /// <summary>
     /// Gets or sets the reference to the existing folder on which the operation will be performed.
     /// </summary>
-    [JsonProperty("reference", Required = Required.Always)]
+    [JsonProperty("reference")]
     public Reference Reference { get; set; }
 
 }
