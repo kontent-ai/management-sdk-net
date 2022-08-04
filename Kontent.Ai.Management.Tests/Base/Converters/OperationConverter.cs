d42eaa7a9eb55d01c4d10cdff4c58c799f97d36e
Jiri Kotasek <kotas24@gmail.com>
2022-08-04 11:13:36 +0200

    fix rebase

---
+++ Kontent.Ai.Management.Tests/Base/Converters/OperationConverter.cs
@@ -0,0 +1,44 @@
+﻿using Kontent.Ai.Management.Models.Types.Patch;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Kontent.Ai.Management.Tests.Base.Converters.ConverterHelper;
+
+namespace Kontent.Ai.Management.Tests.Base.Converters;
+
+internal class ContentTypeOperationBaseModelConverter : JsonConverter
+{
+    public override bool CanConvert(Type objectType) => IsCollectionOf<ContentTypeOperationBaseModel>(objectType);
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        var result = new List<ContentTypeOperationBaseModel>();
+
+        var jArray = JArray.Load(reader);
+
+        var operationTypeMap = typeof(ContentTypeOperationBaseModel)
+            .Assembly.GetTypes()
+            .Where(t => t.IsSubclassOf(typeof(ContentTypeOperationBaseModel)) && !t.IsAbstract)
+            .Select(t =>
+            {
+                var instance = (ContentTypeOperationBaseModel)Activator.CreateInstance(t);
+                return (Key: instance.Op, Type: t);
+            }).ToDictionary(tuple => tuple.Key, tuple => tuple.Type);
+
+        foreach (var item in jArray)
+        {
+            var operation = item["op"].ToString();
+
+            result.Add((ContentTypeOperationBaseModel)item.ToObject(operationTypeMap[operation]));
+        }
+
+        return result;
+    }
+
+    public override bool CanWrite => false;
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) => throw new NotImplementedException();
+}
+
