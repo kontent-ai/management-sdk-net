41aead972328e270866ea509f74cc8742fd91b18
jirik2 <jirik2@kontent.ai>
2023-01-26 14:06:11 +0100

    projectInformation new unit tests

---
+++ Kontent.Ai.Management.Tests/ManagementClientTests/ProjectInformationTests.cs
@@ -0,0 +1,34 @@
+﻿using Kontent.Ai.Management.Tests.Base;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using static Kontent.Ai.Management.Tests.Base.Scenario;
+
+namespace Kontent.Ai.Management.Tests.ManagementClientTests;
+
+public class ProjectInformationTests
+{
+    private readonly Scenario _scenario;
+
+    public ProjectInformationTests()
+    {
+        _scenario = new Scenario(folder: "ProjectInformation");
+    }
+
+    [Fact]
+    public async Task GetProjectInformationAsync_GetsProjectInformationAsync()
+    {
+        var client = _scenario
+            .WithResponses("Project.json")
+            .CreateManagementClient();
+
+        var response = await client.GetProjectInformationAsync();
+
+        _scenario
+            .CreateExpectations()
+            .HttpMethod(HttpMethod.Get)
+            .Response(response)
+            .Url($"{Endpoint}/projects/{PROJECT_ID}")
+            .Validate();
+    }
+}
