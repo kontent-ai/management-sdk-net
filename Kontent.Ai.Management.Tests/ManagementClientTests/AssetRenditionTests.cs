99778efadfe77c5efea50faeeab584d7165b9007
jirik2 <jirik2@kontent.ai>
2023-03-09 14:44:38 +0100

    new unit tests for renditions

--- Kontent.Ai.Management.Tests/ManagementClientTests/AssetRenditionTests.cs
+++ Kontent.Ai.Management.Tests/ManagementClientTests/AssetRenditionTests.cs
@@ -1,79 +1,120 @@
 ﻿using FluentAssertions;
-using FluentAssertions.Execution;
+using Kontent.Ai.Management.Extensions;
 using Kontent.Ai.Management.Models.AssetRenditions;
 using Kontent.Ai.Management.Models.Shared;
 using Kontent.Ai.Management.Tests.Base;
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
+using static Kontent.Ai.Management.Tests.Base.Scenario;
 
 namespace Kontent.Ai.Management.Tests.ManagementClientTests;
 
-public class AssetRenditionTests : IClassFixture<FileSystemFixture>
+public class AssetRenditionTests
 {
-    private readonly FileSystemFixture _fileSystemFixture;
+    private readonly Scenario _scenario;
 
-    public AssetRenditionTests(FileSystemFixture fileSystemFixture)
+    public AssetRenditionTests()
     {
-        _fileSystemFixture = fileSystemFixture;
-        _fileSystemFixture.SetSubFolder("AssetRendition");
-    }
-
-    [Theory]
-    [MemberData(nameof(AssetReferenceRenditionReferenceCombinations))]
-    public async Task GetRenditionAsync_ByCombinationOfInternalAndExternalIds_ReturnsRendition(Reference assetReference, Reference renditionReference)
-    {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRendition.json");
-
-        var expected = _fileSystemFixture.GetExpectedResponse<AssetRenditionModel>("AssetRendition.json");
-
-        var identifier = new AssetRenditionIdentifier(assetReference, renditionReference);
-
-        var response = await client.GetAssetRenditionAsync(identifier);
-
-        response.Should().BeEquivalentTo(expected);
+        _scenario = new Scenario(folder: "AssetRendition");
     }
 
     [Fact]
-    public async Task GetRenditionAsync_IdentifierIsNull_Throws()
+    public async Task ListAssetRenditionsAsync_ById_ReturnsRenditions()
     {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRendition.json");
+        var client = _scenario
+            .WithResponses("AssetRenditionPage1.json", "AssetRenditionPage2.json", "AssetRenditionPage3.json")
+            .CreateManagementClient();
 
-        await client.Invoking(x => x.GetAssetRenditionAsync(null))
-            .Should().ThrowExactlyAsync<ArgumentNullException>();
+        var identifier = Reference.ById(Guid.NewGuid());
+        var response = await client.ListAssetRenditionsAsync(identifier).GetAllAsync();
+
+        _scenario
+            .CreateExpectations()
+            .HttpMethod(HttpMethod.Get)
+            .ListingResponse(response)
+            .Url($"{Endpoint}/projects/{PROJECT_ID}/assets/{identifier.Id}/renditions")
+            .Validate();
     }
 
-    [Theory]
-    [MemberData(nameof(AssetReferenceCombinations))]
-    public async Task ListRenditionsAsync_ByAssetReference_ReturnsRenditions(Reference assetReference)
+    [Fact]
+    public async Task ListAssetRenditionsAsync_ByCodename_Throws()
     {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRenditions.json");
+        var client = _scenario.CreateManagementClient();
 
-        var expected = _fileSystemFixture.GetItemsOfExpectedListingResponse<AssetRenditionModel>("AssetRenditions.json");
+        await client.Invoking(x => x.ListAssetRenditionsAsync(Reference.ByCodename("codename"))).Should().ThrowAsync<Exception>();
+    }
 
-        var response = await client.ListAssetRenditionsAsync(assetReference);
+    [Fact]
+    public async Task ListAssetRenditionsAsync_ByExternalId_ReturnsRenditions()
+    {
+        var client = _scenario
+            .WithResponses("AssetRenditionPage1.json", "AssetRenditionPage2.json", "AssetRenditionPage3.json")
+            .CreateManagementClient();
 
-        response.ToList().Should().BeEquivalentTo(expected);
+        var identifier = Reference.ByExternalId("externalId");
+        var response = await client.ListAssetRenditionsAsync(identifier).GetAllAsync();
+
+        _scenario
+            .CreateExpectations()
+            .HttpMethod(HttpMethod.Get)
+            .ListingResponse(response)
+            .Url($"{Endpoint}/projects/{PROJECT_ID}/assets/external-id/{identifier.ExternalId}/renditions")
+            .Validate();
     }
 
     [Fact]
     public async Task ListRenditionsAsync_IdentifierIsNull_Throws()
     {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRendition.json");
+        var client = _scenario.CreateManagementClient();
 
-        await client.Invoking(x => x.ListAssetRenditionsAsync(null))
-            .Should().ThrowExactlyAsync<ArgumentNullException>();
+        await client.Invoking(x => x.ListAssetRenditionsAsync(null)).Should().ThrowExactlyAsync<ArgumentNullException>();
     }
 
     [Theory]
-    [MemberData(nameof(AssetReferenceCombinations))]
-    public async Task CreateRenditionAsync_ByAssetReference_CreatesRenditions(Reference assetReference)
+    [ClassData(typeof(CombinationOfValidIdentifiersAndUrl))]
+    public async Task GetRenditionAsync_ReturnsRendition(AssetRenditionIdentifier identifier, string expectedUrl)
     {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRendition.json");
+        var client = _scenario
+            .WithResponses("AssetRendition.json")
+            .CreateManagementClient();
 
-        var expected = _fileSystemFixture.GetExpectedResponse<AssetRenditionModel>("AssetRendition.json");
+        var response = await client.GetAssetRenditionAsync(identifier);
+
+        _scenario
+            .CreateExpectations()
+            .HttpMethod(HttpMethod.Get)
+            .Response(response)
+            .Url(expectedUrl)
+            .Validate();
+    }
+
+    [Fact]
+    public async Task GetRenditionAsync_ByCodename_Throws()
+    {
+        var client = _scenario.CreateManagementClient();
+
+        var identifier = new AssetRenditionIdentifier(Reference.ByCodename("assetcodename"), Reference.ByCodename("renditioncodename"));
+        await client.Invoking(x => x.GetAssetRenditionAsync(identifier)).Should().ThrowAsync<Exception>();
+    }
+
+    [Fact]
+    public async Task GetRenditionAsync_IdentifierIsNull_Throws()
+    {
+        var client = _scenario.CreateManagementClient();
+
+        await client.Invoking(x => x.GetAssetRenditionAsync(null)).Should().ThrowExactlyAsync<ArgumentNullException>();
+    }
+
+    [Fact]
+    public async Task CreateAssetRenditionAsync_ById_CreatesRenditions()
+    {
+        var client = _scenario
+            .WithResponses("AssetRendition.json")
+            .CreateManagementClient();
 
         var createModel = new AssetRenditionCreateModel
         {
@@ -89,57 +130,93 @@ public class AssetRenditionTests : IClassFixture<FileSystemFixture>
             }
         };
 
-        var response = await client.CreateAssetRenditionAsync(assetReference, createModel);
+        var identifier = Reference.ById(Guid.NewGuid());
+        var response = await client.CreateAssetRenditionAsync(identifier, createModel);
 
-        using (new AssertionScope())
+        _scenario
+            .CreateExpectations()
+            .HttpMethod(HttpMethod.Post)
+            .RequestPayload(createModel)
+            .Response(response)
+            .Url($"{Endpoint}/projects/{PROJECT_ID}/assets/{identifier.Id}/renditions")
+            .Validate();
+    }
+
+    [Fact]
+    public async Task CreateAssetRenditionAsync_ByCodename_Throws()
+    {
+        var client = _scenario
+            .WithResponses("AssetRendition.json")
+            .CreateManagementClient();
+
+        var identifier = Reference.ByCodename("codename");
+        var createRenditionModel = new AssetRenditionCreateModel();
+
+        await client.Invoking(x => x.CreateAssetRenditionAsync(identifier, createRenditionModel)).Should().ThrowAsync<Exception>();
+    }
+
+    [Fact]
+    public async Task CreateAssetRenditionAsync_ByExternalId_CreatesRenditions()
+    {
+        var client = _scenario
+            .WithResponses("AssetRendition.json")
+            .CreateManagementClient();
+
+        var createModel = new AssetRenditionCreateModel
         {
-            response.Transformation.Should().BeEquivalentTo(createModel.Transformation);
-            response.ExternalId.Should().BeEquivalentTo(expected.ExternalId);
-            response.AssetId.Should().Be(expected.AssetId);
-            response.RenditionId.Should().Be(expected.RenditionId);
-        }
+            ExternalId = "rendition-1",
+            Transformation = new RectangleResizeTransformation
+            {
+                CustomWidth = 120,
+                CustomHeight = 240,
+                X = 300,
+                Y = 200,
+                Width = 360,
+                Height = 720,
+            }
+        };
+
+        var identifier = Reference.ByExternalId("externalId");
+        var response = await client.CreateAssetRenditionAsync(identifier, createModel);
+
+        _scenario
+            .CreateExpectations()
+            .HttpMethod(HttpMethod.Post)
+            .RequestPayload(createModel)
+            .Response(response)
+            .Url($"{Endpoint}/projects/{PROJECT_ID}/assets/external-id/{identifier.ExternalId}/renditions")
+            .Validate();
     }
 
     [Fact]
     public async Task CreateRenditionAsync_IdentifierIsNull_Throws()
     {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRendition.json");
-        var createModel = new AssetRenditionCreateModel
-        {
-            ExternalId = "rendition-1",
-            Transformation = new RectangleResizeTransformation
-            {
-                CustomWidth = 120,
-                CustomHeight = 240,
-                X = 300,
-                Y = 200,
-                Width = 360,
-                Height = 720,
-            }
-        };
+        var client = _scenario
+            .WithResponses("AssetRendition.json")
+            .CreateManagementClient();
 
-        await client.Invoking(x => x.CreateAssetRenditionAsync(null, createModel))
-            .Should().ThrowExactlyAsync<ArgumentNullException>();
+        var createRenditionModel = new AssetRenditionCreateModel();
+
+        await client.Invoking(x => x.CreateAssetRenditionAsync(null, createRenditionModel)).Should().ThrowExactlyAsync<ArgumentNullException>();
     }
 
     [Fact]
     public async Task CreateRenditionAsync_CreateModelIsNull_Throws()
     {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRendition.json");
+        var client = _scenario.CreateManagementClient();
 
-        await client.Invoking(x => x.CreateAssetRenditionAsync(Reference.ByExternalId("asset-1"), null))
-            .Should().ThrowExactlyAsync<ArgumentNullException>();
+        await client.Invoking(x => x.CreateAssetRenditionAsync(Reference.ByExternalId("asset-1"), null)).Should().ThrowExactlyAsync<ArgumentNullException>();
     }
 
     [Theory]
-    [MemberData(nameof(AssetReferenceRenditionReferenceCombinations))]
-    public async Task UpdateRenditionAsync_ByCombinationOfInternalAndExternalIds_UpdatesRenditions(Reference assetReference, Reference renditionReference)
+    [ClassData(typeof(CombinationOfValidIdentifiersAndUrl))]
+    public async Task UpdateAssetRenditionAsync_UpdatesRenditions(AssetRenditionIdentifier identifier, string expectedUrl)
     {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRendition.json");
+        var client = _scenario
+            .WithResponses("AssetRendition.json")
+            .CreateManagementClient();
 
-        var expected = _fileSystemFixture.GetExpectedResponse<AssetRenditionModel>("AssetRendition.json");
-
-        var updateModel = new AssetRenditionUpdateModel()
+        var updateRenditionModel = new AssetRenditionUpdateModel()
         {
             Transformation = new RectangleResizeTransformation
             {
@@ -152,66 +229,76 @@ public class AssetRenditionTests : IClassFixture<FileSystemFixture>
             }
         };
 
-        var response = await client.UpdateAssetRenditionAsync(new AssetRenditionIdentifier(assetReference, renditionReference), updateModel);
+        var response = await client.UpdateAssetRenditionAsync(identifier, updateRenditionModel);
 
-        using (new AssertionScope())
-        {
-            response.Transformation.Should().BeEquivalentTo(updateModel.Transformation);
-            response.ExternalId.Should().BeEquivalentTo(expected.ExternalId);
-            response.AssetId.Should().Be(expected.AssetId);
-            response.RenditionId.Should().Be(expected.RenditionId);
-        }
+        _scenario
+            .CreateExpectations()
+            .HttpMethod(HttpMethod.Put)
+            .RequestPayload(updateRenditionModel)
+            .Response(response)
+            .Url(expectedUrl)
+            .Validate();
     }
 
     [Fact]
-    public async Task UpdateRenditionAsync_IdentifierIsNull_Throws()
+    public async Task UpdateAssetRenditionAsync_ByCodename_Throws()
     {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRendition.json");
-        var updateModel = new AssetRenditionUpdateModel
-        {
-            Transformation = new RectangleResizeTransformation
-            {
-                CustomWidth = 120,
-                CustomHeight = 240,
-                X = 300,
-                Y = 200,
-                Width = 360,
-                Height = 720,
-            }
-        };
+        var client = _scenario.CreateManagementClient();
 
-        await client.Invoking(x => x.UpdateAssetRenditionAsync(null, updateModel))
-            .Should().ThrowExactlyAsync<ArgumentNullException>();
+        var identifier = new AssetRenditionIdentifier(Reference.ByCodename("assetcodename"), Reference.ByCodename("renditioncodename"));
+        var updateRenditionModel = new AssetRenditionUpdateModel();
+
+        await client.Invoking(x => x.UpdateAssetRenditionAsync(identifier, updateRenditionModel)).Should().ThrowAsync<Exception>();
+    }
+
+    [Fact]
+    public async Task UpdateAssetRenditionAsync_IdentifierIsNull_Throws()
+    {
+        var client = _scenario.CreateManagementClient();
+
+        var updateRenditionModel = new AssetRenditionUpdateModel();
+
+        await client.Invoking(x => x.UpdateAssetRenditionAsync(null, updateRenditionModel)).Should().ThrowExactlyAsync<ArgumentNullException>();
     }
 
     [Fact]
     public async Task UpdateRenditionAsync_CreateModelIsNull_Throws()
     {
-        var client = _fileSystemFixture.CreateMockClientWithResponse("AssetRendition.json");
+        var client = _scenario.CreateManagementClient();
 
-        var identifier = new AssetRenditionIdentifier(Reference.ByExternalId("asset-1"), Reference.ByExternalId("rendition-1"));
-
-        await client.Invoking(x => x.UpdateAssetRenditionAsync(identifier, null))
-            .Should().ThrowExactlyAsync<ArgumentNullException>();
+        var identifier = new AssetRenditionIdentifier(Reference.ById(Guid.NewGuid()), Reference.ById(Guid.NewGuid()));
+        await client.Invoking(x => x.UpdateAssetRenditionAsync(null, null)).Should().ThrowExactlyAsync<ArgumentNullException>();
     }
 
-    public static IEnumerable<object[]> AssetReferenceRenditionReferenceCombinations
+    private class CombinationOfValidIdentifiersAndUrl : IEnumerable<object[]>
     {
-        get
-        {
-            yield return new object[] { Reference.ById(Guid.NewGuid()), Reference.ById(Guid.NewGuid()) };
-            yield return new object[] { Reference.ByExternalId("asset-1"), Reference.ById(Guid.NewGuid()) };
-            yield return new object[] { Reference.ById(Guid.NewGuid()), Reference.ByExternalId("rendition-1") };
-            yield return new object[] { Reference.ByExternalId("asset-1"), Reference.ByExternalId("rendition-1") };
-        }
-    }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-    public static IEnumerable<object[]> AssetReferenceCombinations
-    {
-        get
+        public IEnumerator<object[]> GetEnumerator()
         {
-            yield return new object[] { Reference.ById(Guid.NewGuid()) };
-            yield return new object[] { Reference.ByExternalId("asset-1") };
+            foreach (var (Identifier, Url) in GetPermutation())
+            {
+                yield return new object[] { Identifier, Url };
+            }
         }
+
+        public IEnumerable<(AssetRenditionIdentifier Identifier, string Url)> GetPermutation()
+        {
+            var assetIdentifier = new[] { ById, ByExternalId };
+            var renditionIdentifiers = new[] { ById, ByExternalId };
+
+            foreach (var item in assetIdentifier)
+            {
+                foreach (var language in renditionIdentifiers)
+                {
+                    var identifier = new AssetRenditionIdentifier(item.Identifier, language.Identifier);
+                    var url = $"{Endpoint}/projects/{PROJECT_ID}/assets/{item.UrlSegment}/renditions/{language.UrlSegment}";
+                    yield return (identifier, url);
+                }
+            }
+        }
+
+        private static (Reference Identifier, string UrlSegment) ById => (Reference.ById(Guid.Parse("4b628214-e4fe-4fe0-b1ff-955df33e1515")), "4b628214-e4fe-4fe0-b1ff-955df33e1515");
+        private static (Reference Identifier, string UrlSegment) ByExternalId => (Reference.ByExternalId("external-id"), "external-id/external-id");
     }
 }
