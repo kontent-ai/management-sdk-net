03a5014401d73f1387175a97d322c8dda612b5f5
tomasw_kontent <tomasw@kontent.ai>
2024-01-24 08:56:16 +0100

    Add new webhook filters

---
+++ Kontent.Ai.Management/Models/Webhooks/Triggers/Taxonomy/TaxonomyFiltersModel.cs
@@ -0,0 +1,17 @@
+﻿using Kontent.Ai.Management.Models.Shared;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Kontent.Ai.Management.Models.Webhooks.Triggers.Taxonomy;
+
+/// <summary>
+/// Represents the taxonomy filters model.
+/// </summary>
+public class TaxonomyFiltersModel
+{
+    /// <summary>
+    /// References to taxonomies
+    /// </summary>
+    [JsonProperty("taxonomies")]
+    public IEnumerable<Reference> Taxonomies { get; set; }
+}
