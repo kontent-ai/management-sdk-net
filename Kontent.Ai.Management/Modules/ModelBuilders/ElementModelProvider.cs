9cadb1153cc38acdfd6914db4b042177d5bafaa5
Daniel Pokorny <ext_daniel.pokorny@kontent.ai>
2025-06-04 13:13:41 +0200

    make ElementModelProvider public

--- Kontent.Ai.Management/Modules/ModelBuilders/ElementModelProvider.cs
+++ Kontent.Ai.Management/Modules/ModelBuilders/ElementModelProvider.cs
@@ -7,8 +7,12 @@ using System.Reflection;
 
 namespace Kontent.Ai.Management.Modules.ModelBuilders;
 
-internal class ElementModelProvider : IElementModelProvider
+/// <summary>
+/// Provides methods for converting between dynamic and strongly-typed models.
+/// </summary>
+public class ElementModelProvider : IElementModelProvider
 {
+    /// <inheritdoc />
     public T GetStronglyTypedElements<T>(IEnumerable<dynamic> elements) where T : new()
     {
         var type = typeof(T);
@@ -30,6 +34,7 @@ internal class ElementModelProvider : IElementModelProvider
         return instance;
     }
 
+    /// <inheritdoc />
     public IEnumerable<dynamic> GetDynamicElements<T>(T stronglyTypedElements)
     {
         var type = typeof(T);
